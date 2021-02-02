6c3803b8378e373d00cc211b2000efe69874b239
eeapbh <eeapbh@gmail.com>
2021-02-02 18:08:31 +0900

    [ADD] 그룹게시판, 채팅, 일정, 그룹원 페이지 만듬

---
+++ frontend/src/components/group/GroupChat.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>그룹채팅</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- frontend/src/components/group/GroupNav.vue
+++ frontend/src/components/group/GroupNav.vue
@@ -7,96 +7,38 @@
       <v-tab>그룹원</v-tab>
     </v-tabs>
 
-    <v-tabs-items style="height:800px" v-model="tab">
+    <v-tabs-items v-model="tab">
       <v-tab-item>
-        <v-card flat
-          ><v-simple-table>
-            <template v-slot:default>
-              <tbody>
-                <tr style="height:100px" v-for="item in table" :key="item.name">
-                  <td>{{ item.name }}</td>
-                  <td>{{ item.date }}<br />{{ item.writer }}</td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-card>
+        <v-card flat> <GroupBoard /></v-card>
       </v-tab-item>
       <v-tab-item>
-        <v-card flat>
-          일정
-        </v-card>
+        <v-card flat> <GroupSchedule /></v-card>
       </v-tab-item>
       <v-tab-item>
-        <v-card flat>
-          채팅
-        </v-card>
+        <v-card flat><GroupChat /> </v-card>
       </v-tab-item>
       <v-tab-item>
-        <v-card flat>
-          그룹원
-        </v-card>
+        <v-card flat><GroupMemberList /> </v-card>
       </v-tab-item>
     </v-tabs-items>
   </v-card>
 </template>
 <script>
+import GroupBoard from "@/components/group/GroupBoard.vue";
+import GroupSchedule from "@/components/group/GroupSchedule.vue";
+import GroupChat from "@/components/group/GroupChat.vue";
+import GroupMemberList from "@/components/group/GroupMemberList.vue";
 export default {
+  components: {
+    GroupBoard,
+    GroupSchedule,
+    GroupChat,
+    GroupMemberList,
+  },
+
   data() {
     return {
       tab: null,
-      table: [
-        {
-          name: "<공지> 가입인사양식",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Ice cream sandwich",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Eclair",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Cupcake",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Gingerbread",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Jelly bean",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Lollipop",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Honeycomb",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "Donut",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-        {
-          name: "KitKat",
-          date: "1월 19일 17:13분",
-          writer: "박봉현",
-        },
-      ],
     };
   },
 };
