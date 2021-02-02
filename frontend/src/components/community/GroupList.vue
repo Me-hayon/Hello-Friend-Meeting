079b3328026ef3de0d4f84d0833043d291f3ef51
eeapbh <eeapbh@gmail.com>
2021-02-02 15:36:12 +0900

    [UPDATE] 그룹리스트 수정 그룹db만들고 다시작업예정

--- frontend/src/components/community/GroupList.vue
+++ frontend/src/components/community/GroupList.vue
@@ -1,9 +1,65 @@
 <template>
-  <div>페이지 구현 중</div>
+  <v-simple-table style="margin-top:50px; margin-bottom:70px">
+    <template v-slot:default>
+      <tbody>
+        <tr class="groupTd" v-for="item in groups" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>
+            <i style="margin-right:10px" class="material-icons"
+              ><font-awesome-icon :icon="['far', 'user']"/></i
+            >{{ item.calories }}
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      groups: [
+        {
+          name: "토익스터디",
+          calories: 59,
+        },
+        {
+          name: "스트라이크",
+          calories: 27,
+        },
+        {
+          name: "영화보러가자",
+          calories: 22,
+        },
+        {
+          name: "롤팸",
+          calories: 35,
+        },
+        {
+          name: "토익스터디",
+          calories: 59,
+        },
+        {
+          name: "스트라이크",
+          calories: 27,
+        },
+        {
+          name: "영화보러가자",
+          calories: 22,
+        },
+        {
+          name: "롤팸",
+          calories: 35,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style>
+.groupTd {
+  height: 100px;
+}
+</style>
