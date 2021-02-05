a9375aec2982d7941943baab963dbf9f321ac53d
Baekgol <changu13@naver.com>
2021-02-05 13:17:52 +0900

    [UPDATE] route.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,37 +1,19 @@
-import FriendInfo from '@/views/user/FriendInfo.vue';
-
-<<<<<<< HEAD
-import Login from "@/views/user/Login.vue";
-import Join from "@/views/user/Join.vue";
-import UserInfo from "@/views/user/Info.vue";
-import PasswordModify from "@/views/user/PasswordModify.vue";
-import FeedMain from "@/views/feed/IndexFeed.vue";
-import Community from "@/views/Community.vue";
-import GroupList from "@/components/community/GroupList.vue";
-import FriendList from "@/components/community/FriendList.vue";
-import Message from "@/views/message/Message.vue";
-import ErrorPage from "@/views/Error.vue";
-import PageNotFound from "@/views/PageNotFound.vue";
-import ChangeAvatar from "@/views/user/ChangeAvatar.vue";
-import GroupBoardDetail from "@/components/group/GroupBoardDetail.vue";
-import FriendTest from "@/views/FriendTest.vue";
-=======
 import Login from '@/views/user/Login.vue';
 import Join from '@/views/user/Join.vue';
-import UserInfo from '@/views/user/Info.vue';
 import PasswordModify from '@/views/user/PasswordModify.vue';
 import FeedMain from '@/views/feed/IndexFeed.vue';
 import Community from '@/views/Community.vue';
 import GroupList from '@/components/community/GroupList.vue';
 import FriendList from '@/components/community/FriendList.vue';
-
+import UserInfo from '@/views/user/Info.vue';
+import FriendInfo from '@/views/user/FriendInfo.vue';
+import GroupMainPage from '@/components/group/GroupMainPage.vue';
+import GroupBoardDetail from '@/components/group/GroupBoardDetail.vue';
+import Message from '@/views/message/Message.vue';
 import ErrorPage from '@/views/Error.vue';
 import PageNotFound from '@/views/PageNotFound.vue';
-
 import FriendTest from '@/views/FriendTest.vue';
->>>>>>> refs/heads/FF-00-app-lcg
 
-import GroupMainPage from '@/components/group/GroupMainPage.vue';
 export default [
   {
     path: '/ft/:uno',
@@ -89,13 +71,13 @@ export default [
     ],
   },
   {
-    path: "/message",
-    name: "Message",
+    path: '/message',
+    name: 'Message',
     component: Message,
   },
   {
-    path: "/group",
-    name: "GroupMainPage",
+    path: '/group',
+    name: 'GroupMainPage',
     component: GroupMainPage,
     props: true,
   },
@@ -106,14 +88,14 @@ export default [
     component: ErrorPage,
   },
   {
-    path: "/board/detail",
-    name: "GroupBoardDetail",
+    path: '/board/detail',
+    name: 'GroupBoardDetail',
     component: GroupBoardDetail,
     props: true,
   },
   {
-    path: "*",
-    name: "PageNotFound",
+    path: '*',
+    name: 'PageNotFound',
 
     component: PageNotFound,
   },
