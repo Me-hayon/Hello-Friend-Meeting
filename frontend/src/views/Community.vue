800768d043f7c10f69ddf2939bfbd74011268817
eeapbh <eeapbh@gmail.com>
2021-02-10 16:12:23 +0900

    [UPDATE] group/list 배경색바꾸기

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -1,18 +1,18 @@
 <template>
-  <b-container style="background-color: rgb(247, 246, 232);">
+  <b-container>
     <Tab />
     <router-view />
   </b-container>
 </template>
 
 <script>
-import Tab from '@/components/community/Tab.vue';
+import Tab from "@/components/community/Tab.vue";
 
 export default {
   components: { Tab },
   created() {
-    this.$store.commit('setIsHeader', true);
-    this.$store.commit('setIsFooter', true);
+    this.$store.commit("setIsHeader", true);
+    this.$store.commit("setIsFooter", true);
   },
 };
 </script>
